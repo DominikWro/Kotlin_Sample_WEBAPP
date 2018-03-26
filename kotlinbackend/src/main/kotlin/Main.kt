023ba44ab7267023baafadb6a8da265e8a1041f8
DominikWro <dominik.wronski@gmail.com>
2018-03-26 17:24:56 +0200

    Endpoint ping

--- kotlinbackend/src/main/kotlin/Main.kt
+++ kotlinbackend/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 //Main.kt
+import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
@@ -15,7 +16,21 @@ fun main(args: Array<String>) {
             get("/demo") {
                 call.respondText("HELLO WORLD!")
             }
+            get("/api/ping/{count?}") {
+                var count: Int = Integer.valueOf(call.parameters["count"]?: "1")
+                if (count < 1) {
+                    count = 1
+                }
+                var obj = Array<Entry>(count, {i -> Entry("$i: Hello, World!")})
+                val gson = Gson()
+                var str = gson.toJson(obj)
+                call.response.header("Access-Control-Allow-Origin", "*")
+                call.respondText(str, ContentType.Application.Json)
+
+            }
         }
     }
     server.start(wait = true)
 }
+
+data class Entry(val message: String)
